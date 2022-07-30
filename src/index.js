f6d4e74eb3d5804623631e5ce5adc41fbcf4e807
Daria Approvato <dariaapprovato@gmail.com>
2022-07-29 20:44:49 -0400

    Completed Tasklister Lab

--- src/index.js
+++ src/index.js
@@ -1,3 +1,17 @@
 document.addEventListener("DOMContentLoaded", () => {
-  // your code here
-});
+  document.querySelector('form').addEventListener('submit', (event) => {
+    event.preventDefault()
+    const newTaskDescription = document.getElementById('new-task-description')
+    const newTask = document.createElement('li')
+    const btn = document.createElement('button')
+    btn.addEventListener('click', deleteButton)
+    btn.textContent = 'x'
+    newTask.innerText = newTaskDescription.value
+    newTask.appendChild(btn)
+    document.querySelector('#tasks').appendChild(newTask)
+  })
+})
+
+function deleteButton(event) {
+  event.target.parentNode.remove()
+}
